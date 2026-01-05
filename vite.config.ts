ce833806ed43173b44280909ccc332710539476f
cateiru <git@cateiru.com>
2026-01-05 10:03:11 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  base: '/iframe-poc/',
+  base: "/iframe-recursion-poc/",
   root: ".",
   build: {
     rollupOptions: {
